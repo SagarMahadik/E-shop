f71fc31a33dca0b06e2a2f41a05cc6b5a6adfc13
sgrmhdk51 <sgrmhdk51@gmail.com>
2019-11-02 20:04:01 +0530

    Brolenhomepage into smaller component and styling menus

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import HomePage from './homepage.component'
-import './homepage.styles.scss'
+import HomePage from './pages/homepage/homepage.component'
+
 
 class App extends Component {
   render() {
